3d123489ec7e433623ae1e22df443965c6bd7fe5
Jake Valsamis <jake@codeocean.com>
2023-08-25 21:36:55 +0000

    support for output format

--- code/query.py
+++ code/query.py
@@ -10,12 +10,14 @@ if (len(sys.argv) >= 4):
     host = sys.argv[3]
     port = int(sys.argv[4])
     file_name = sys.argv[5]
+    output_format = sys.argv[6]
 else:
     query = "SELECT * FROM tickit.users"
     database = "sample_data_dev"
     host = "acmecorp-cfn-demo-endpoint-endpoint-tvuxknj0hc5lueze2pff.147080935342.us-east-1.redshift-serverless.amazonaws.com"
     port = 5439
     file_name = "users_table"
+    output_format = "csv"
 
 if host == "Default Host":
     host = "acmecorp-cfn-demo-endpoint-endpoint-tvuxknj0hc5lueze2pff.147080935342.us-east-1.redshift-serverless.amazonaws.com"
@@ -49,7 +51,20 @@ df = cursor.fetch_dataframe()
 
 #preview table and save it as a csv
 print(df.head())
-df.to_csv(f"../results/{file_name}.csv")  
+# df.to_csv(f"../results/{file_name}.csv")  
+match output_format: 
+    case 'tsv': 
+        print("I'm a tsv")
+        df.to_csv(f"../results/{file_name}.tsv", sep='\t')
+    case 'csv': 
+        print("I'm a csv")
+        df.to_csv(f"../results/{file_name}.csv")
+    case 'parquet': 
+        print("I'm a parquet")
+        df.to_parquet(f"../results/{file_name}.parquet")
+    case 'json': 
+        print("I'm a json")
+        df.to_json(f"../results/{file_name}.json")
 
 #close connection
 conn.close()
